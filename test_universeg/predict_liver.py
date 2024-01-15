bc198252d53d62e2755a97a3973ab3fd2ef7dd51
alina.ryabtsev <alina.ryabtsev@gmail.com>
2024-01-15 13:07:35 +0200

    added liver prediction with universeg using 128 by 128 patches

--- test_universeg/predict_liver.py
+++ test_universeg/predict_liver.py
@@ -1,4 +1,5 @@
 import sys
+from typing import List
 
 sys.path.append("/cs/casmip/alina.ryabtsev/FewShotLearning/")
 import torch
@@ -14,7 +15,7 @@ import numpy as np
 from tqdm import tqdm
 from skimage.transform import resize
 import nibabel as nib
-
+from skimage import measure
 sys.path.append("/cs/casmip/alina.ryabtsev/Tools")
 import logging
 from tqdm.contrib.logging import logging_redirect_tqdm
@@ -22,7 +23,10 @@ import gc
 
 sys.path.append('UniverSeg')
 
+# PATCH_SIZE = (64, 64)  # old patch size
 PATCH_SIZE = (128, 128)
+K_SHOTS = 6
+HALF_PRECISION = False
 
 
 # Dice metric for measuring volume agreement
@@ -35,13 +39,15 @@ def dice_score(y_pred: torch.Tensor, y_true: torch.Tensor) -> float:
     return float(format(2. * intersection.sum() / (y_true.sum() + y_pred.sum()), '.3f'))
 
 
-def load_data() -> tuple[LiverTumorsDataset3D, LiverTumorsDataset3D]:
+def load_data(half_precision: bool = False) -> tuple[LiverTumorsDataset3D, LiverTumorsDataset3D]:
     """
     This function loads the data from the dataset
     :return: support and query sets
     """
-    d_support = LiverTumorsDataset3D(split="support", support_frac=0.05, label=1, resize_scan=False)
-    d_query = LiverTumorsDataset3D(split="query", support_frac=0.95, label=1, resize_scan=False)
+    d_support = LiverTumorsDataset3D(split="support", support_frac=0.1, label=1, resize_scan=False,
+                                     half_precision=half_precision)
+    d_query = LiverTumorsDataset3D(split="query", support_frac=0.1, label=1, resize_scan=False,
+                                   half_precision=half_precision)
     return d_support, d_query
 
 
@@ -59,13 +65,39 @@ def get_support_images(d_support: LiverTumorsDataset3D, K: int = 5) -> tuple[
     return support_images, support_labels, support_filenames
 
 
+def get_positive_patches_idx(masks: torch.Tensor) -> List[int]:
+    # Label connected components in the mask
+    positive_masks = []
+    for i, mask in enumerate(masks):
+        mask = mask.cpu().numpy()
+        labels = measure.label(mask, background=0)
+
+        # # Create an empty mask to store the connected components not on the boundaries
+        # result_mask = np.zeros_like(mask)
+
+        # Loop through each labeled region
+        for region in measure.regionprops(labels):
+            # Check if the bounding box is entirely within the mask boundaries
+            if all(point > 0 and point < size for point, size in zip(region.bbox[1:3] + region.bbox[4:], PATCH_SIZE*2)):
+                # Draw the region on the result mask
+                # result_mask[labels == region.label] = 1
+                if region.area > 30:
+                    positive_masks.append(i)
+
+        # if result_mask.sum() != 0:
+        #     positive_masks.append(i)
+
+    return np.unique(positive_masks)
+
+
 def get_support_patches(support_images: torch.Tensor, support_labels: torch.Tensor,
                         patch_size: tuple[int, int] = (64, 64)) -> tuple[torch.Tensor, torch.Tensor]:
     splitter = SlidingWindowSplitter(patch_size=patch_size, overlap=0.5, device=device)
     # eliminate all negative patches
     images_patches = torch.concat([t[0] for t in splitter(support_images[0])]).to(device)
     labels_patches = torch.concat([t[0] for t in splitter(support_labels[0])]).to(device)
-    positive_patches_idx = [i for i, l in enumerate(labels_patches) if l.sum() > 0]
+    # positive_patches_idx = [i for i, l in enumerate(labels_patches) if l.sum() > 50]
+    positive_patches_idx = get_positive_patches_idx(labels_patches)
     labels_patches = labels_patches[positive_patches_idx].to(device)
     images_patches = images_patches[positive_patches_idx].to(device)
 
@@ -131,19 +163,22 @@ def main():
     model = universeg(pretrained=True)
     _ = model.to(device)
     print(f"Running on device: {device}")
-    # model = model.half()  # 16-bit precision
-    with torch.cuda.amp.autocast():
-        _extracted_from_main_10(model)
+    d_support, d_query = load_data(HALF_PRECISION)
+    if HALF_PRECISION:
+        model = model.half()
+        with torch.cuda.amp.autocast():  # 16-bit precision
+            print("Running with autocast mode (16-bit precision)")
+            _extracted_from_main_10(model, d_support, d_query)
+    else:
+        _extracted_from_main_10(model, d_support, d_query)
 
 
-def _extracted_from_main_10(model: nn.Module):
+def _extracted_from_main_10(model: nn.Module, d_support: LiverTumorsDataset3D, d_query: LiverTumorsDataset3D):
     """
     Helper function for main
     :param model: universeg loaded model running on cuda and half precision
     """
-    print("Running with autocast mode (16-bit precision)")
-    d_support, d_query = load_data()
-    support_images, support_labels, support_filenames = get_support_images(d_support, K=3)
+    support_images, support_labels, support_filenames = get_support_images(d_support, K=K_SHOTS)
     support_images_patches, support_labels_patches = get_support_patches(support_images[None], support_labels[None],
                                                                          patch_size=PATCH_SIZE)
     slice_inferer = SliceInferer(spatial_dim=0, roi_size=(512, 512), sw_batch_size=1, progress=True, device=device)
@@ -161,7 +196,7 @@ def _extracted_from_main_10(model: nn.Module):
                 hard_pred = res["Prediction"]
                 assert not torch.any(torch.isnan(hard_pred)), f"Prediction contains NaNs: {scan_name}"
                 logging.info(f"finished inference of scan {i + 1}: {scan_name}")
-                preprocess_prediction(hard_pred, seg_name, True, save_name="patches16bit", resize_scan=False)
+                preprocess_prediction(hard_pred, seg_name, True, save_name=f"patches_k{K_SHOTS}", resize_scan=False)
                 logging.info(f"finished postprocessing of prediction {i + 1}: {scan_name}")
                 torch.cuda.empty_cache()
                 gc.collect()
