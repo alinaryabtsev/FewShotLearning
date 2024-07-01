89278bc720ea045db54b81683c7b5eabf90d29d9
alina.ryabtsev <alina.ryabtsev@gmail.com>
2024-07-01 12:00:27 +0300

    same patient would not be in the support set twice or more

--- Lemonade/datasets_loaders/dataset_utils.py
+++ Lemonade/datasets_loaders/dataset_utils.py
@@ -5,8 +5,11 @@ import nibabel as nib
 from skimage.transform import resize
 from tqdm import tqdm
 import einops as E
-# import the constants from the constants.py file
-from .. import constants
+# import the constants from the constants.py file that in parent directory
+import sys
+sys.path.append("..")
+import constants
+# from .. import constants
 
 
 def preprocess_scan_and_segmentation(scan, segmentation, split, scan_resize, clip_values, half_precision=False):
@@ -22,7 +25,8 @@ def preprocess_scan_and_segmentation(scan, segmentation, split, scan_resize, cli
     scan_data = (scan_data - scan_data.min()) / (scan_data.max() - scan_data.min())
     if scan_resize:
         scan_data = resize(scan_data, (*constants.RESIZE_RESOLUTION, scan_data.shape[2]))
-        seg_data = resize(seg_data, (*constants.RESIZE_RESOLUTION, seg_data.shape[2]), order=0, preserve_range=True, anti_aliasing=False)
+        seg_data = resize(seg_data, (*constants.RESIZE_RESOLUTION, seg_data.shape[2]), order=0, preserve_range=True,
+                          anti_aliasing=False)
     seg_data = (seg_data >= 1).astype(seg_data.dtype)
     scan_data = E.rearrange(scan_data, "H W D ->  D 1 H W")
     seg_data = E.rearrange(seg_data, "H W D ->  D 1 H W")
@@ -45,13 +49,21 @@ def preprocess_scan_and_segmentation(scan, segmentation, split, scan_resize, cli
 
 
 def load_scans(split, split_i, N, path, resize_scan=True, half_precision=False):
-    rng = np.random.default_rng(42)
+    rng = np.random.default_rng(constants.RANDOM_SEED)
     p = rng.permutation(N)
     data = []
-    scans = sorted(glob(os.path.join(path, f"*{constants.SCANS_FORMAT}")))
-    scans = [scans[i] for i in p]
-    segmentations = sorted(glob(os.path.join(path, f"*{constants.SEGMENTATIONS_FORMAT}")))
-    segmentations = [segmentations[i] for i in p]
+    scans_files = sorted(glob(os.path.join(path, f"*{constants.SCANS_FORMAT}")))
+    scans_per = [scans_files[i] for i in p]
+    # do not allow to have two scans or more of the same patient in the support set
+    scans = []
+    while len(scans) <= split_i:
+        cur_scan = scans_per.pop(0)
+        if os.path.basename(cur_scan)[:-22] not in [os.path.basename(s)[:-22] for s in scans]:
+            scans.append(cur_scan)
+        else:
+            scans_per.append(cur_scan)
+    scans.extend(scans_per)
+    segmentations = [s.replace(constants.SCANS_FORMAT, constants.SEGMENTATIONS_FORMAT) for s in scans]
 
     if split == "support":
         iter_data = lambda: zip(scans[:split_i], segmentations[:split_i])
--- Lemonade/datasets_loaders/liver_metastasis_dataset_3D.py
+++ Lemonade/datasets_loaders/liver_metastasis_dataset_3D.py
@@ -6,7 +6,10 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 from .dataset_utils import load_scans
-from ..constants import LIVER_LESIONS_DATASET, SCANS_FORMAT
+# from ..constants import LIVER_LESIONS_DATASET, SCANS_FORMAT
+import sys
+sys.path.append("..")
+import constants
 
 
 @dataclass
@@ -14,15 +17,15 @@ class LiverTumorsDataset3D(Dataset):
     """Creates a dataset from CASMIP's liver metastasis training set"""
     split: Literal["support", "query", "query-support"]
     label: int
-    dataset_path: str = LIVER_LESIONS_DATASET
-    support_frac: float = 0.7
+    dataset_path: str = constants.LIVER_LESIONS_DATASET
+    support_frac: float = constants.SUPPORT_FRAC
     resize_scan: bool = True
     half_precision: bool = False
 
     def __post_init__(self):
         # arrange data: self.data = [(img1, seg1), (img2, seg2) ...]
         # get number of items in folder:
-        N = len(glob(os.path.join(self.dataset_path, f"*{SCANS_FORMAT}")))  # directory contains both scans and segmentations.
+        N = len(glob(os.path.join(self.dataset_path, f"*{constants.SCANS_FORMAT}")))  # directory contains both scans and segmentations.
         self.split_i = int(np.floor(self.support_frac * N))
 
         T = torch.from_numpy
